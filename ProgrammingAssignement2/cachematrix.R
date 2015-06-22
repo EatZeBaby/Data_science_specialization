1ad54f7ac099f5f2f7f84861a831bd5516b72f4f
Axel <axel.richier@gmail.com>
2015-06-22 23:04:18 +0200

    cacheMatrix added

---
+++ ProgrammingAssignement2/cachematrix.R
@@ -0,0 +1,44 @@
+setwd("~/Documents/MOOCS/R/ProgrammingAssignment2")
+
+## Put comments here that give an overall description of what your
+## functions do
+
+
+## The first function, makeVector creates a special "vector", which is really a list containing a function to
+## set the value of the matrix
+## get the value of the matrix
+## set the value of the inverse of the matrix
+## get the value of the inverse of the matrix
+makeCacheMatrix <- function(x = matrix()) {
+	inv <- null
+	
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+
+}
+
+
+## Write a short comment describing this function
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
+cachemean <- function(x, ...) {
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinv(inv)
+        inv
+}
